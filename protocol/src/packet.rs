194a3a51cac84d370a927e6c3117ebf9c6c57a3a
piggsoft <piggsoft@163.com>
2024-03-01 08:20:11 +0000

    添加协议修改

--- protocol/src/packet.rs
+++ protocol/src/packet.rs
@@ -1,26 +1,52 @@
 use bytes::{Buf, Bytes};
-use crate::data_type;
-use crate::data_type::ParseError;
-
 
 pub fn get_unit_le_by_length(bytes: &mut Bytes, n_bytes: usize) -> Result<u64, ParseError> {
     if bytes.remaining() < n_bytes {
-        Err(ParseError::LengthNotEnough {
-            expect: n_bytes,
-            actual: bytes.remaining(),
-        })
+        Err(
+            ParseError::new(
+                format!("expect size is {}, actual size is {}", n_bytes, bytes.remaining()),
+                ParseErrorKind::LengthNotEnough,
+            )
+        )
     } else {
         Ok(bytes.get_uint_le(n_bytes))
     }
 }
 
-
-
+#[derive(strum_macros::Display)]
 enum ParseData {
     Int(u64),
-    Str(String),
+    Str(Bytes),
     Error(String),
     Null,
+}
+
+struct ParseError {
+    message: String,
+    kind: ParseErrorKind,
+}
+
+#[derive(strum_macros::Display)]
+enum ParseErrorKind {
+    LengthNotEnough,
+    SystemError,
+}
+
+impl ParseError {
+    fn new(message: String,
+           kind: ParseErrorKind) -> Self {
+        Self {
+            message,
+            kind,
+        }
+    }
+
+    fn new_form_str(message: String) -> Self {
+        Self {
+            message,
+            kind: ParseErrorKind::SystemError,
+        }
+    }
 }
 
 trait ToParseData {
@@ -29,7 +55,7 @@ trait ToParseData {
 
 impl ToParseData for u64 {
     fn to_parse_data(self) -> ParseData {
-       ParseData::Int(self)
+        ParseData::Int(self)
     }
 }
 
@@ -41,20 +67,38 @@ pub fn parse_int_lenenc(bytes: &mut Bytes) -> Result<ParseData, ParseError> {
         0xFC => get_unit_le_by_length(bytes, 2)?.to_parse_data(),
         0xFD => get_unit_le_by_length(bytes, 3)?.to_parse_data(),
         0xFE => get_unit_le_by_length(bytes, 8)?.to_parse_data(),
-        0xFF => ParseData::Error(String::from("error")),
-        _ => ParseData::Error(String::from("error")),
+        0xFF => ParseData::Error("error".to_string()),
+        _ => ParseData::Error("error".to_string()),
     };
     Ok(data)
-
 }
 
-pub fn parse_str_lenenc(bytes: &mut Bytes, n_bytes: usize) -> Result<ParseData, ParseError> {
+pub fn parse_str_lenenc(bytes: &mut Bytes) -> Result<ParseData, ParseError> {
     let string_length = parse_int_lenenc(bytes)?;
+
     if let ParseData::Int(string_length) = string_length {
         let string_bytes = bytes.split_to(string_length as usize);
-        String::from_utf8(string_bytes.to_vec())
+        Ok(ParseData::Str(string_bytes))
+    } else {
+        Err(ParseError::new_form_str(format!("error int<lenenc> {}", string_length)))
     }
-    Ok(ParseData::Null)
+}
+
+fn read_packet(bytes: &mut Bytes) -> Result<Bytes, ParseError> {
+    let payload_length = bytes.get_uint_le(3) as usize;
+    let sequence_id = bytes.get_u8();
+    let data = if payload_length == 0 {
+        Bytes::new()
+    } else {
+        let mut _data = bytes.split_to(payload_length);
+
+        if payload_length == 0xFF_FF_FF {
+            let mut chain = _data.chain(read_packet(bytes)?);
+            _data = chain.copy_to_bytes(chain.remaining())
+        }
+        _data
+    };
+    Ok(data)
 }
 
 struct Packet {
@@ -64,9 +108,9 @@ struct Packet {
 }
 
 enum Payload {
-    OK,
-    ERR,
-    EOF,
+    OK(OkPacket),
+    ERR(ErrPacket),
+    EOF(String),
 }
 
 struct OkPacket {
@@ -125,3 +169,51 @@ enum OkPacketArgs {
 //         Ok(Packet { payload_length: 1, sequence_id: 1, payload: Payload::OK })
 //     }
 // }
+
+#[cfg(test)]
+mod test {
+    use bytes::{Buf, Bytes};
+
+    use crate::packet::read_packet;
+
+    #[test]
+    fn test_read_packet() {
+        let mut data: Vec<u8> = vec![
+            0b1111_1111,
+            0b1111_1111,
+            0b1111_1111,
+            0b0000_0001,
+            //0b0000_00001,
+        ];
+        for n in 1..=16_777_215 {
+            data.push(0b0000_0001);
+        }
+        data.push(0b0000_0001);
+        data.push(0b0000_0000);
+        data.push(0b0000_0000);
+        data.push(0b0000_0001);
+        data.push(0b0000_0001);
+
+        let mut bytes = Bytes::from(data);
+        let bytes = read_packet(&mut bytes);
+        if let Ok(mut bytes) = bytes {
+            assert_eq!(16_777_216, bytes.remaining())
+        }
+    }
+
+    #[test]
+    fn test_read_packet_01() {
+        let mut data: Vec<u8> = vec![
+            0b0000_0001,
+            0b0000_0000,
+            0b0000_0000,
+            0b0000_0001,
+            0b0000_0001,
+        ];
+        let mut bytes = Bytes::from(data);
+        let bytes = read_packet(&mut bytes);
+        if let Ok(mut bytes) = bytes {
+            assert_eq!(0b0000_0001, bytes.get_u8())
+        }
+    }
+}
